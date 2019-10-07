b6b9bc50d9279e287e09cf8ad5967a0ec782caed
kim-ly <SoSe2018>
2019-10-07 22:23:20 +0200

    Finished project

---
+++ app/src/main/java/com/example/userprofile/CreateProfileActivity.kt
@@ -0,0 +1,63 @@
+package com.example.userprofile
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_create_profile.*
+
+class CreateProfileActivity : AppCompatActivity() {
+
+    private var profileImageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+        initViews()
+    }
+    private fun initViews() {
+        button.setOnClickListener { onGalleryClick() }
+        button2.setOnClickListener { onConfirmClick() }
+    }
+
+    private fun onConfirmClick() {
+        val profile = Profile(
+            etFirstName.text.toString(),
+            etLastName.text.toString(),
+            etDescription.text.toString(),
+            profileImageUri
+        )
+
+        val profileActivityIntent = Intent(this, ProfileActivity::class.java)
+        profileActivityIntent.putExtra(ProfileActivity.PROFILE_EXTRA, profile)
+        startActivity(profileActivityIntent)
+    }
+
+
+    private fun onGalleryClick() {
+        // Create an Intent with action as ACTION_PICK
+        val galleryIntent = Intent(Intent.ACTION_PICK)
+
+        // Sets the type as image/*. This ensures only components of type image are selected
+        galleryIntent.type = "image/*"
+
+        // Start the activity using the gallery intent
+        startActivityForResult(galleryIntent, GALLERY_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                GALLERY_REQUEST_CODE -> {
+                    profileImageUri = data?.data
+                    imageView2.setImageURI(profileImageUri)
+                }
+            }
+        }
+    }
+    companion object {
+        const val GALLERY_REQUEST_CODE = 100
+    }
+}
